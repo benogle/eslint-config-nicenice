c8d845f05f394a727909ff875bb58818cb89614e
Derek Foster <derekfoster94@gmail.com>
2023-01-11 10:23:26 -0800

    only allow mocha global env in tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,109 +1,114 @@
 'use strict'
 
 module.exports = {
-  parser: '@babel/eslint-parser',
+  parser: "@babel/eslint-parser",
   parserOptions: {
     // This is to disable requiring a babel config file be found
     requireConfigFile: false,
   },
-  extends: ['standard', 'standard-jsx', 'plugin:prettier/recommended'],
-  plugins: ['no-only-tests', 'prettier', 'react-camel-case'],
+  extends: ["standard", "standard-jsx", "plugin:prettier/recommended"],
+  plugins: ["no-only-tests", "prettier", "react-camel-case"],
   rules: {
-    'jsx-quotes': ['error', 'prefer-double'],
-    'object-curly-spacing': ['error', 'always'],
+    "jsx-quotes": ["error", "prefer-double"],
+    "object-curly-spacing": ["error", "always"],
 
-    'import/no-unresolved': ['error', { commonjs: true, caseSensitive: true }],
+    "import/no-unresolved": ["error", { commonjs: true, caseSensitive: true }],
 
-    'react/prop-types': 'warn',
+    "react/prop-types": "warn",
 
-    'react/sort-comp': [
-      'error',
+    "react/sort-comp": [
+      "error",
       {
         order: [
-          'static-methods',
-          'instance-variables',
-          'lifecycle',
-          '/^handle.+$/',
-          'getters',
-          'setters',
-          '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
-          'instance-methods',
-          'everything-else',
-          'rendering',
+          "static-methods",
+          "instance-variables",
+          "lifecycle",
+          "/^handle.+$/",
+          "getters",
+          "setters",
+          "/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/",
+          "instance-methods",
+          "everything-else",
+          "rendering",
         ],
         groups: {
           lifecycle: [
-            'displayName',
-            'propTypes',
-            'contextTypes',
-            'childContextTypes',
-            'mixins',
-            'statics',
-            'defaultProps',
-            'constructor',
-            'getDefaultProps',
-            'getInitialState',
-            'state',
-            'getChildContext',
-            'componentWillMount',
-            'componentDidMount',
-            'componentWillReceiveProps',
-            'shouldComponentUpdate',
-            'componentWillUpdate',
-            'componentDidUpdate',
-            'componentWillUnmount',
+            "displayName",
+            "propTypes",
+            "contextTypes",
+            "childContextTypes",
+            "mixins",
+            "statics",
+            "defaultProps",
+            "constructor",
+            "getDefaultProps",
+            "getInitialState",
+            "state",
+            "getChildContext",
+            "componentWillMount",
+            "componentDidMount",
+            "componentWillReceiveProps",
+            "shouldComponentUpdate",
+            "componentWillUpdate",
+            "componentDidUpdate",
+            "componentWillUnmount",
           ],
-          rendering: ['/^render.+$/', 'render'],
+          rendering: ["/^render.+$/", "render"],
         },
       },
     ],
 
     // This is for the mocha tests. It should be an override, but they dont work for me
     // http://eslint.org/docs/user-guide/configuring#configuration-based-on-glob-patterns
-    'no-unused-expressions': 0,
-    'no-only-tests/no-only-tests': 'error',
+    "no-unused-expressions": 0,
+    "no-only-tests/no-only-tests": "error",
 
-    'arrow-parens': ['error', 'always'], // https://eslint.org/docs/rules/arrow-parens
-    'comma-dangle': [
-      'error',
+    "arrow-parens": ["error", "always"], // https://eslint.org/docs/rules/arrow-parens
+    "comma-dangle": [
+      "error",
       {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'ignore',
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "ignore",
       },
     ],
-    semi: ['error', 'never'],
-    'space-before-function-paren': ['error', 'always'],
+    semi: ["error", "never"],
+    "space-before-function-paren": ["error", "always"],
 
     // Disallow prettier errors to show as eslint warnings
-    'prettier/prettier': 'off',
+    "prettier/prettier": "off",
 
     // These need to change to errors and be fixed through the code
-    'array-callback-return': ['warn', { checkForEach: true }],
-    'no-prototype-builtins': 'warn',
-    'react/jsx-handler-names': 'off',
-    'react/no-deprecated': 'warn',
-    'prefer-regex-literals': 'warn',
-    'import/no-named-default': 'off',
-    'import/named': 2,
+    "array-callback-return": ["warn", { checkForEach: true }],
+    "no-prototype-builtins": "warn",
+    "react/jsx-handler-names": "off",
+    "react/no-deprecated": "warn",
+    "prefer-regex-literals": "warn",
+    "import/no-named-default": "off",
+    "import/named": 2,
 
-    'react-camel-case/react-camel-case': 'error',
+    "react-camel-case/react-camel-case": "error",
   },
+  overrides: [
+    {
+      files: ["**/tests/**", "**/spec.js", "**/__mocks/**"],
+      env: { mocha: true, jest: true, jasmine: true },
+    },
+  ],
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
   env: {
     browser: true,
     node: true,
-    mocha: true,
   },
   globals: {
     sinon: true,
     expect: true,
     chai: true,
   },
-}
+};
